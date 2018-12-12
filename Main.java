1c1349e1224df89bd1cbe367c7cd643afbba8ea5
Jaylukmann <33833198+Jaylukmann@users.noreply.github.com>
2018-12-12 20:44:49 +0000

    Create Main.java
    This is paid educational game in form of an App where people all over the world commit some sum of money in order to play the game. If the player is versed in the concerned topic he wins more than he has committed otherwise,he gets kicked in the butt and tempted to play more after carrying out his research.The game encourages proper study. It is planned to be  world wide but we are starting with Nigerian history.

---
+++ Main.java
@@ -0,0 +1,110 @@
+/* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nigerianhistorygame;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jayluk
+ */
+public class Nigerianhistorygame {
+     static int j[]={1,2,3,4};
+
+    /**
+     * @param args the command line arguments
+     */
+      
+     
+          public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("----------------------WELCOME------------------------");
+        System.out.println("----------THIS IS A GAME ABOUT NIGERIAN HISTORY----------");     
+        System.out.println("---------------------------------------------------------"); 
+        System.out.println("\tTHIS SECTION CONTAINS 20 QUESTIONS"); 
+        System.out.println("\tTYPE IN THE CORRESPONDING NUMBER WITH THE CORRECT ANSWER"); 
+        Scanner input = new Scanner(System.in);
+        System.out.println("(1)Nigeria got her independence from great Britain in ______________?\t(1)1961\t(2)1960\t(3)1956\t(4)1966\t");
+        int m = input.nextInt();
+        if(  m != j[1]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON #500");
+        System.out.println("(2)The Northern and southern protectorate was amalgamated in ___________?\t(1)1915\t(2)1906\t(3)1914\t(4)1916\t");
+         int n = input.nextInt();
+        if(  n != j[2]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #1000 IN YOUR WALLET ALREADY");
+              System.out.println("(3)Nigeria got her name from river___________?\t(1)Benue\t(2)Kanji\t(3)Nile\t(4)Niger\t");
+            int o = input.nextInt();
+        if(  o != j[3]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #1500 IN YOUR WALLET ALREADY");
+            System.out.println("(4)The name of the first president of Nigeria is___________?\t(1)Nnamdi Azikwe\t(2)Tafawa Balewa\t(3)Obafemi Awolowo\t(4)Ladoke Akintola\t");
+            int p = input.nextInt();
+        if(  p != j[0]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #2000 IN YOUR WALLET ALREADY");
+             System.out.println("(5)How many political parties were registered for the 1999 general election?\t(1)Two\t(2)Three\t(3)Four\t(4)Five\t");
+            int q= input.nextInt();
+        if(  q != j[1]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #2500 IN YOUR WALLET ALREADY");
+              System.out.println("(6)How many political parties were registered for the 1993 general election?\t(1)Two\t(2)Three\t(3)Four\t(4)Five\t");
+            int r= input.nextInt();
+        if(  r != j[0]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #3000 IN YOUR WALLET ALREADY");
+             System.out.println("(7)Nigeria witness her first fuel scarcity in __________\t(1)1966\t(2)1967\t(3)1970\t(4)1977\t");
+            int s= input.nextInt();
+        if(  s != j[3]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #3500 IN YOUR WALLET ALREADY");
+            System.out.println("(8)War Against Indiscipline was launched by Buhari/Idiagbon regime on the 20th Of March, __________\t(1)1984\t(2)1985\t(3)1983\t(4)2015\t");
+            int t= input.nextInt();
+        if(  t != j[0]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #4000 IN YOUR WALLET ALREADY");
+            System.out.println("(9)Attempt was made to kidnapp Umaru Dikko back to Nigeria from London to come and face Corruption charges by Buhari's government on the  __________1985\t(1)1st of June\t(2)1st of April\t(3)1st of December\t(4)1st of May\t");
+            int u= input.nextInt();
+        if(  u != j[1]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #4500 IN YOUR WALLET ALREADY");
+            System.out.println("(10)The Federal Capital Territory was moved from Lagos to Abuja on the 12th of December__________\t(1)1991\t(2)1992\t(3)1993\t(4)1994\t");
+            int v= input.nextInt();
+        if(  v != j[0]){
+            System.out.println("SORRY,YOU ARE NOT CORRECT");
+                }else{
+            System.out.println("GREAT!,YOU JUST WON EXTRA #500");
+            System.out.println("YOU HAVE #5000 IN YOUR WALLET ALREADY");
+        }}
+        }
+        }
+        }
+        }
+        }
+        }
+          }
+}
+}
+}
+
+
+
